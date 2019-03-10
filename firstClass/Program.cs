959766b88a65130904d83fb724eca9e61fd4a49f
Gjurgica Anastasovska <gurgica78@gmail.com>
2019-03-10 12:44:02 +0100

    First commit

---
+++ firstClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student("Gjurgica", "SEDC", "G2");
+            students[1] = new Student("Gorjan", "Code Academy", "G1");
+            students[2] = new Student("Ana", "Code Academy", "G1");
+            students[3] = new Student("Bojan", "SEDC", "G2");
+            students[4] = new Student("Dario", "SEDC", "G1");
+            Console.WriteLine("Enter a student name");
+            string answer = Console.ReadLine();
+            bool studentFound = false;
+            while(studentFound == false)
+            {
+                for (int i = 0; i < students.Length; i++)
+                {
+                    if (answer == students[i].Name)
+                    {
+                        Console.WriteLine($"{students[i].Name} is student in {students[i].Academy} in {students[i].Group} group.");
+                        studentFound = true;
+                    }
+                }
+                if(studentFound == false)
+                {
+                    Console.WriteLine("This student not exist");
+                    answer = Console.ReadLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ firstClass/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstClass
+{
+    public class Student
+    {
+        public Student(string name,string academy,string group)
+        {
+            Name = name;
+            Academy = academy;
+            Group = group;
+        }
+        public string Name { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+    }
+}
